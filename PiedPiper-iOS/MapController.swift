02ca42d9e5a1f1dabc1570380c81572a9b70f0ef
Pranav Bokey <pranavbokey@Pranavs-MacBook-Pro.local>
2017-12-02 23:28:09 -0500

    finished map view

--- PiedPiper-iOS/MapController.swift
+++ PiedPiper-iOS/MapController.swift
@@ -10,22 +10,33 @@ import UIKit
 import GoogleMaps
 
 class MapController: UIViewController {
+    var sightingList = [RatSighting]()
     
     override func loadView() {
         super.loadView()
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        //var mapView = MapView(frame: <#T##CGRect#>(x: 0, y: 0))
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        // var mapView = MapView(frame: <#T##CGRect#>(x: 0, y: 0))
+        let list = RatSightingList()
+        list.databaseListUpdate { (arr) in
+            self.sightingList = arr
+            let camera = GMSCameraPosition.camera(withLatitude: Double(self.sightingList[0].latitude)!, longitude: Double(self.sightingList[0].longitude)!, zoom: 10.0)
+            let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+            self.view = mapView
+            for item in self.sightingList {
+                print(item.latitude)
+                print(item.longitude)
+                let lat = Double(item.latitude)
+                let lon = Double(item.longitude)
+                if (lat != nil && lon != nil) {
+                    let marker = GMSMarker()
+                    marker.position = CLLocationCoordinate2D(latitude: lat!, longitude: lon!)
+                    marker.title = item.uniqueKey
+                    marker.snippet = item.createdDate
+                    marker.map = mapView
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
